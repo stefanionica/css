d815521b0700a360f2f473490e25ca3c352199da
unknown <stefan.ionica2000@gmail.com>
2022-03-10 20:40:05 +0200

    css

---
+++ curs4/js/app.js
@@ -0,0 +1,14 @@
+function creatediv(n){
+    for (var h = 0; h < n; h++) {
+        var div = document.createElement('div');    
+        // div.innerHTML = h+1;
+        div.className = 'box';
+        document.getElementById("wrapper").appendChild(div);
+        
+    }
+    
+}
+creatediv(72);
+
+
+
